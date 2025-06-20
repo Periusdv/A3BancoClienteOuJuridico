7af496314d2fb9c6fa1e29150d49bdb6513033d0
GarraOne <garrra.one@gmail.com>
2025-06-20 16:45:36 -0300

    refactor: eliminar duplicação de código

--- src/main/java/aula/AulaServico.java
+++ src/main/java/aula/AulaServico.java
@@ -1,48 +1,47 @@
 package aula;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class AulaServico {
-    
-    private Random random = new Random();
-    
+
+    private SecureRandom random = new SecureRandom();
+
     public PessoaJuridica criarPessoaJuridica(String nome, String cnpj, int numAgencia, boolean criarCorrente, double salario, boolean criarPoupanca, double rendimento) {
-       
+
         ArrayList<Conta> contas = new ArrayList<>();
         Agencia agencia = new Agencia(numAgencia);
         PessoaJuridica pessoajuridica = new PessoaJuridica(nome, contas, cnpj);
-        
-        
+
         if (criarCorrente) {
             ContaCorrente contacorrente = new ContaCorrente(random.nextInt(100), salario, agencia);
             pessoajuridica.setListaContas(contacorrente);
         }
-        
+
         if (criarPoupanca) {
             ContaPoupanca contapoupanca = new ContaPoupanca(random.nextInt(100), rendimento, agencia);
             pessoajuridica.setListaContas(contapoupanca);
         }
-        
+
         return pessoajuridica;
     }
-    
+
     public PessoaFisica criarPessoaFisica(String nome, String cpf, int numAgencia, boolean criarCorrente, double salario, boolean criarPoupanca, double rendimento) {
-        
+
         ArrayList<Conta> contas = new ArrayList<>();
         Agencia agencia = new Agencia(numAgencia);
         PessoaFisica pessoafisica = new PessoaFisica(nome, contas, cpf);
-                
+
         if (criarCorrente) {
             ContaCorrente contacorrente = new ContaCorrente(random.nextInt(100), salario, agencia);
             pessoafisica.setListaContas(contacorrente);
         }
-        
+
         if (criarPoupanca) {
             ContaPoupanca contapoupanca = new ContaPoupanca(random.nextInt(100), rendimento, agencia);
             pessoafisica.setListaContas(contapoupanca);
         }
-        
+
         return pessoafisica;
     }
 }
--- src/main/java/aula/AulaUI.java
+++ src/main/java/aula/AulaUI.java
@@ -4,71 +4,76 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class AulaUI {
-    
+
     private static final Logger LOGGER = Logger.getLogger(AulaUI.class.getName());
-    
+
+    private static class DadosConta {
+
+        final boolean criarCorrente;
+        final double salario;
+        final boolean criarPoupanca;
+        final double rendimento;
+
+        DadosConta(boolean criarCorrente, double salario, boolean criarPoupanca, double rendimento) {
+            this.criarCorrente = criarCorrente;
+            this.salario = salario;
+            this.criarPoupanca = criarPoupanca;
+            this.rendimento = rendimento;
+        }
+    }
+
+    private DadosConta processoConta(Scanner input) {
+
+        LOGGER.info("Deseja criar uma Conta Corrente? 1-Sim | 2-Não");
+        boolean criarCorrente = input.nextInt() == 1;
+
+        double salario = 0;
+        if (criarCorrente) {
+            LOGGER.info("Informe o Salário: ");
+            salario = Double.parseDouble(input.next().replace(",", "."));
+        }
+
+        LOGGER.info("Deseja criar uma Conta Poupança? 1-Sim | 2-Não");
+        boolean criarPoupanca = input.nextInt() == 1;
+
+        double rendimento = 0;
+        if (criarPoupanca) {
+            LOGGER.info("Informe o Rendimento: ");
+            rendimento = Double.parseDouble(input.next().replace(",", "."));
+        }
+        return new DadosConta(criarCorrente, salario, criarPoupanca, rendimento);
+
+    }
+
     public void executar() {
         Scanner input = new Scanner(System.in);
         AulaServico aulaServico = new AulaServico();
-        
+
         LOGGER.info("Informe qual agencia você deseja criar a conta. Agencias Disponíveis: 1, 2 e 3");
         int numAgencia = input.nextInt();
-        
+
         LOGGER.info("Informe o nome da Pessoa: ");
         String nome = input.next();
-        
+
         LOGGER.info("Deseja criar a conta para pessoa Jurídica ou Fisíca: \n Digite \n 1: Jurídica \n 2: Física");
         int tipoPessoa = input.nextInt();
-        
+
         if (tipoPessoa == 1) {
             LOGGER.info("Informe o CNPJ: ");
             String cnpj = input.next();
-            
-            LOGGER.info("Deseja criar uma Conta Corrente? 1-Sim | 2-Não");
-            boolean criarCorrente = input.nextInt() == 1;
-            
-            double salario = 0;
-            if (criarCorrente) {
-                LOGGER.info("Informe o Salário: ");
-                salario = Double.parseDouble(input.next().replace(",", "."));
-            }
-            
-            LOGGER.info("Deseja criar uma Conta Poupança? 1-Sim | 2-Não");
-            boolean criarPoupanca = input.nextInt() == 1;
-            
-            double rendimento = 0;
-            if (criarPoupanca) {
-                LOGGER.info("Informe o Rendimento: ");
-                rendimento = Double.parseDouble(input.next().replace(",", "."));
-            }
-            
-            aulaServico.criarPessoaJuridica(nome, cnpj, numAgencia, criarCorrente, salario, criarPoupanca, rendimento);
-            
+
+            DadosConta dados = processoConta(input);
+
+            aulaServico.criarPessoaJuridica(nome, cnpj, numAgencia, dados.criarCorrente, dados.salario, dados.criarPoupanca, dados.rendimento);
         } else if (tipoPessoa == 2) {
             LOGGER.info("Informe o CPF: ");
             String cpf = input.next();
-            
-            LOGGER.info("Deseja criar uma Conta Corrente? 1-Sim | 2-Não");
-            boolean criarCorrente = input.nextInt() == 1;
-            
-            double salario = 0;
-            if (criarCorrente) {
-                LOGGER.info("Informe o Salário: ");
-                salario = Double.parseDouble(input.next().replace(",", "."));
-            }
-            
-            LOGGER.info("Deseja criar uma Conta Poupança? 1-Sim | 2-Não");
-            boolean criarPoupanca = input.nextInt() == 1;
-            
-            double rendimento = 0;
-            if (criarPoupanca) {
-                LOGGER.info("Informe o Rendimento: ");
-                rendimento = Double.parseDouble(input.next().replace(",", "."));
-            }
-            
-            aulaServico.criarPessoaFisica(nome, cpf, numAgencia, criarCorrente, salario, criarPoupanca, rendimento);
+
+            DadosConta dados = processoConta(input);
+
+            aulaServico.criarPessoaFisica(nome, cpf, numAgencia, dados.criarCorrente, dados.salario, dados.criarPoupanca, dados.rendimento);
         }
-        
+
         input.close();
     }
 }
